72334b9d0962711767979255ad6a3e1367a4b7d5
Chip Castle <chip@chipcastle.com>
2009-07-17 15:49:23 -0500

    Finally got a Braintree SUCCESS for add_recurring

--- lib/active_merchant/billing/gateways/braintree.rb
+++ lib/active_merchant/billing/gateways/braintree.rb
@@ -4,7 +4,7 @@ module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class BraintreeGateway < Gateway
       include BraintreeCommon
-      
+
       def self.new(options={})
         if options.has_key?(:login)
           BraintreeOrangeGateway.new(options)
@@ -12,6 +12,27 @@ module ActiveMerchant #:nodoc:
           BraintreeBlueGateway.new(options)
         end
       end
+
+      def add_recurring(money, creditcard, options = {})
+        require 'logger'
+        logger = Logger.new("/tmp/smart_ps.log")
+
+        post = {}
+        add_invoice(post, options)
+        add_payment_source(post, creditcard,options)
+        add_address(post, options[:billing_address]||options[:address])
+        add_address(post, options[:shipping_address],"shipping")
+        add_customer_data(post, options)
+        add_start_date(post, options)
+        add_sku(post, options)
+        logger.info "post=#{post}\nmoney=#{money}\ncreditcard=#{creditcard}\noptions=#{options.to_yaml}"
+
+        commit('add_recurring', money, post)
+      end
+      self.supported_countries = ['US']
+      self.supported_cardtypes = [:visa, :master, :american_express, :discover]
+      self.homepage_url = 'http://www.braintreepaymentsolutions.com'
+      self.display_name = 'Braintree'
     end
   end
 end
